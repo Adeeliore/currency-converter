b8b94e79959c51a6ee87a4dd7fdd13a954f71dbb
Adeeliore <116266434+Adeeliore@users.noreply.github.com>
2024-05-17 17:03:55 +0300

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,15 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import {Component, createComponent} from '@angular/core';
+import {AppModule} from "./app.module";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrls: ['./app.component.css'],
+  imports: [
+    AppModule
+  ]
 })
 export class AppComponent {
-  title = 'converter';
+  title = 'currency-converter';
 }
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,7 @@
 import { Routes } from '@angular/router';
+import { ConverterComponent } from './converter/converter.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: '/converter', pathMatch: 'full' }, // Перенаправление на /converter по умолчанию
+  { path: 'converter', component: ConverterComponent } // Маршрут для отображения ConverterComponent по URL /converter
+];
